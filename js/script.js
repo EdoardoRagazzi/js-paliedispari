59cd99a506f146f40a5da08c646edc669d73e636
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-07 15:39:10 +0200

    Palindroma

--- js/script.js
+++ js/script.js
@@ -49,3 +49,21 @@ function random(min, max) {
      */
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+// Palidroma Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
+
+var parola = prompt('inserisci la parola');
+
+var parolaInversa = invertiParola(parola);
+
+if (parola == parolaInversa) {
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+}
+
+function invertiParola(str) {
+    var strInversa = str.split('').reverse().join('');
+    return strInversa;
+}
